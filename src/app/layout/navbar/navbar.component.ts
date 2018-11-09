dfc118a0f204d06a86ddb96322e75ef39af73362
cloessen <s.claussen@me.com>
2018-11-09 10:12:41 +0100

    added navigation, removed layout module & component

---
+++ src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+  // public isCollapsed = false;
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  signOutHandler() {
+      alert('erfolgreich abgemeldet, bis zum nächsten mal');
+  }
+
+}
