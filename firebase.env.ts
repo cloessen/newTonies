cac22a3261055baac64b0e35e364c3ddc96b137f
cloessen <s.claussen@me.com>
2018-11-16 09:49:16 +0100

    Delete firebase.env.ts

--- firebase.env.ts
+++
@@ -1,8 +0,0 @@
-export const firebase = {
-  apiKey: 'AIzaSyDWxoGAlUcc4j5MQU7-q_epsahrzi-yCiQ',
-  authDomain: 'meine-tonies.firebaseapp.com',
-  databaseURL: 'https://meine-tonies.firebaseio.com',
-  projectId: 'meine-tonies',
-  storageBucket: 'meine-tonies.appspot.com',
-  messagingSenderId: '155276706891'
-};
